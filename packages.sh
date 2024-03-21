ebf7af2b86d16e73af9e7b0a7614223502698481
jifollan <jifollan@stud.ntnu.no>
2024-03-21 14:14:03 +0100

    added packages for Samba

--- packages.sh
+++ packages.sh
@@ -2,4 +2,4 @@
 
 sudo apt update
 
-sudo apt install vim git surf python3-django build-essential xserver-xorg-core xinit xinput x11-xserver-utils libx11-dev libxinerama-dev libxft-dev
+sudo apt install vim git surf python3-django build-essential samba cifs-utils smbclient xserver-xorg-core xinit xinput x11-xserver-utils libx11-dev libxinerama-dev libxft-dev
