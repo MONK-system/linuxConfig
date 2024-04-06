22eacf8f1bf00cafa4b6f4c666d2818471fbd1bf
jonafoll <jonathan.folland@gmail.com>
2024-04-06 12:32:09 +0200

    Upadted installscript

--- install.sh
+++ install.sh
@@ -2,29 +2,29 @@
 
 echo "Starting script..."
 
-# Checks if script is running as root
-if [ "$(id -u)" -ne 0 ]; then
-	echo "This script must be run as root" 1>&2
+# Checks if script is not running as root
+if [ "$(id -u)" -eq 0 ]; then
+	echo "This script must not be run as root" 1>&2
 	exit 1
 fi
 
 echo "Running the packages script..."
 # Runs the packages script
-./linuxConfig/packages.sh
+#sudo /home/$USER/linuxConfig/packages.sh
 
 echo "Creating ~/.config directory..."
 mkdir ~/.config
 
 echo "Cloning and configuring DWM..."
 # Installs and configures DWM
-git clone https://git.suckless.org/dwm ~/.config/dwm
-cp ~/.linuxConfig/config.h ~/.config/dwm
+sudo git clone https://git.suckless.org/dwm /home/$USER/.config/dwm
+sudo cp /home/$USER/.linuxConfig/config.h ~/.config/dwm
 cd ~/.config/dwm
-make clean install
+sudo make clean install
 
 echo "Appending content to /etc/apparmor.d/usr.bin.surf..."
 # Append content to /etc/apparmor.d/usr.bin.surf
-content=$(cat linuxConfig/usr.bin.surf)
+content=$(cat ~/linuxConfig/usr.bin.surf)
 sed -i '${/}/s/$/'"$content"'/' /etc/apparmor.d/usr.bin.surf
 
 echo "Reloading AppArmor profile..."
@@ -37,15 +37,18 @@ mv ~/linuxConfig/.xinitrc ~/
 
 echo "Appending content to ~/.profile..."
 # Append content to ~/.profile
-cat ~/linuxConfig/.profile >>~/.profile
+sudo cat /home/$USER/linuxConfig/.profile >>/home/$USER/.profile
 
 echo "Cloning and setting up MONK system..."
 # Cloning and setting up MONK system
-git clone https://github.com/MONK-system/system ~/
+sudo git clone https://github.com/MONK-system/system /home/$USER/
+
+echo "Switching to the 'lib' branch in the system repository..."
+cd ~/system
+sudo git checkout lib
 
 echo "Creating virtual environment and installing gunicorn..."
 # Creating virtual environment and installing gunicorn
-cd ~/system
 virtualenv monkenv
 source monkenv/bin/activate
 pip install gunicorn
