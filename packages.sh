2bae22324ba99325efa9f9aef9df02865c235798
jonafoll <jonathan.folland@gmail.com>
2024-04-06 13:34:53 +0200

    Added virtualenv package

--- packages.sh
+++ packages.sh
@@ -2,4 +2,4 @@
 
 sudo apt update
 
-sudo apt install vim git surf python3-django build-essential nginx samba cifs-utils smbclient xserver-xorg-core xinit xinput x11-xserver-utils libx11-dev libxinerama-dev libxft-dev
+sudo apt install vim git surf python3-django build-essential nginx virtualenv python3-pip samba cifs-utils smbclient xserver-xorg-core xinit xinput x11-xserver-utils libx11-dev libxinerama-dev libxft-dev
