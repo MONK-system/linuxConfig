ba31feeadaf89bfa3e7c822bdbbcd4100653fd82
jonafoll <jonathan.folland@gmail.com>
2024-04-18 14:31:15 +0200

    Reverted some changes

--- install.sh
+++ install.sh
@@ -47,9 +47,9 @@ echo "Cloning and setting up MONK system..."
 # Cloning and setting up MONK system
 sudo git clone https://github.com/MONK-system/system $HOME/system
 
-echo "Switching to the 'lib' branch in the system repository..."
+echo "Switching to the 'dev' branch in the system repository..."
 cd $HOME/system
-sudo git checkout lib
+sudo git checkout dev
 
 echo "Fixing permissions for database"
 sudo chmod 660 $HOME/system/monksystem/db.sqlite3
@@ -72,4 +72,4 @@ pip install django
 pip install git+https://github.com/MONK-system/library.git
 
 echo "Script completed. Restart to run MonkSystem"
-#source $HOME/.profile
+source $HOME/.profile
