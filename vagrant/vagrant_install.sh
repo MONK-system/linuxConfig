df85751e7a9a59012d9e566dd1bb14c2e8ac5306
jonafoll <jonathan.folland@gmail.com>
2024-05-22 14:16:39 +0200

    Updated vagrant

--- vagrant/vagrant_install.sh
+++ vagrant/vagrant_install.sh
@@ -2,9 +2,15 @@
 
 echo "Starting script..."
 
-su - vagrant <<'EOF'
+# Update package list and install necessary packages with --force-yes
+sudo apt-get update
+sudo apt-get install -y --force-yes git make nginx samba virtualenv apparmor-utils expect
+
 echo "Switched to the Vagrant user..."
+sudo -u vagrant -H sh <<'EOF'
 echo "Cloning linuxConfig repository..."
+
+# Install git to clone the repository
 git clone https://github.com/MONK-system/linuxConfig ~/linuxConfig/
 
 echo "Running the packages script..."
@@ -12,82 +18,67 @@ echo "Running the packages script..."
 yes | ~/linuxConfig/scripts/packages.sh
 
 echo "Creating ~/.config directory..."
-mkdir $HOME/.config
+mkdir -p ~/.config
 
 echo "Cloning and configuring DWM..."
 # Installs and configures DWM
-sudo git clone https://git.suckless.org/dwm $HOME/.config/dwm
-sudo cp $HOME/linuxConfig/config/config.h $HOME/.config/dwm
-cd $HOME/.config/dwm
+git clone https://git.suckless.org/dwm ~/.config/dwm
+cp ~/linuxConfig/config/config.h ~/.config/dwm
+cd ~/.config/dwm
 sudo make clean install
 
+EOF
+
 echo "Appending content to /etc/apparmor.d/usr.bin.surf..."
-sudo su -c '
-          cat /home/vagrant/linuxConfig/config/usr.bin.surf > /etc/apparmor.d/usr.bin.surf
+sudo bash -c 'cat /home/vagrant/linuxConfig/config/usr.bin.surf > /etc/apparmor.d/usr.bin.surf'
 
-          echo "Setting up Samba File Share"
-          sudo mkdir /samba_share
-          sudo cat /home/vagrant/linuxConfig/config/smb.conf >>/etc/samba/smb.conf
-          sudo groupadd smbshare
-          sudo chgrp -R smbshare /samba_share
-          sudo chmod 2770 /samba_share
+echo "Setting up Samba File Share"
+sudo mkdir -p /samba_share
+sudo bash -c 'cat /home/vagrant/linuxConfig/config/smb.conf >> /etc/samba/smb.conf'
+sudo groupadd smbshare
+sudo chgrp -R smbshare /samba_share
+sudo chmod 2770 /samba_share
 
-          # Use expect to automatically provide the Samba password
-          expect <<'EOD'
-          sudo smbpasswd -a $USER
-          expect "New SMB password:"
-          send "123\r"
-          expect "Retype new SMB password:"
-          send "123\r"
-          expect eof
-          EOD
+# Use expect to automatically provide the Samba password
+sudo expect <<EOD
+spawn sudo smbpasswd -a vagrant
+expect "New SMB password:"
+send "123\r"
+expect "Retype new SMB password:"
+send "123\r"
+expect eof
+EOD
 
-          sudo systemctl restart nmbd
-' root
+sudo systemctl restart smbd
 
 echo "Reloading AppArmor profile..."
-# Reload AppArmor profile
 sudo apparmor_parser -r /etc/apparmor.d/usr.bin.surf
 
 echo "Moving .xinitrc file..."
-# Move .xinitrc file
-sudo mv $HOME/linuxConfig/config/.xinitrc $HOME/
+sudo mv /home/vagrant/linuxConfig/config/.xinitrc /home/vagrant/
 
 echo "Appending content to ~/.profile..."
-# Append content to ~/.profile
-sudo cat $HOME/linuxConfig/config/.profile >>$HOME/.profile
+sudo bash -c 'cat /home/vagrant/linuxConfig/config/.profile >> /home/vagrant/.profile'
 
-sudo mv $HOME/linuxConfig/config/monkserver /etc/nginx/sites-available/
+sudo mv /home/vagrant/linuxConfig/config/monkserver /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/monkserver /etc/nginx/sites-enabled/
 sudo systemctl restart nginx
 
 echo "Cloning and setting up MONK system..."
-# Cloning and setting up MONK system
-sudo git clone https://github.com/MONK-system/system $HOME/system
+sudo -u vagrant -H sh <<'EOF'
+git clone https://github.com/MONK-system/system ~/system
+cd ~/system
+git checkout main
 
-echo "Switching to the 'main' branch in the system repository..."
-cd $HOME/system
-sudo git checkout main
-#
-# echo "Fixing permissions for database"
-# sudo chmod 660 $HOME/system/monksystem/db.sqlite3
-# sudo chmod 775 $HOME/system/monksystem/nihon_kohden_files/
-sudo chmod 775 $HOME/system/monksystem/
-#
-# sudo chown $USER:$USER $HOME/system/monksystem/db.sqlite3
-# sudo chown $USER:$USER $HOME/system/monksystem/nihon_kohden_files/
-sudo chown $USER:$USER $HOME/system/monksystem/
+echo "Fixing permissions for database"
+sudo chmod 775 ~/system/monksystem/
+sudo chown $USER:$USER ~/system/monksystem/
 
 echo "Creating virtual environment and installing gunicorn..."
-# Creating virtual environment and installing gunicorn
-sudo virtualenv monkenv
-sudo chown -R $USER:$USER $HOME/system/monkenv
-sudo chmod -R u+w $HOME/system/monkenv
-
-source $HOME/system/monkenv/bin/activate
-pip install gunicorn django plotly numpy pandas git+https://github.com/MONK-system/library.git
-python3 $HOME/system/monksystem/manage.py migrate
+virtualenv ~/system/monkenv
+source ~/system/monkenv/bin/activate
+~/system/monkenv/bin/pip install gunicorn django plotly numpy pandas git+https://github.com/MONK-system/library.git
+~/system/monkenv/bin/python3 ~/system/monksystem/manage.py migrate
 
 echo "Script completed. Restart to run MonkSystem"
-
 EOF
